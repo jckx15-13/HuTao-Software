90c4694acb25c7c424cbe53f5c90b0d62f38add0
Sakuyo Kurosa <sakuyo.mitsukawa14@gmail.com>
2026-08-07 19:03:23 +0800

    chore: add storybook eslint plugin and update gitignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:jsx-a11y/recommended',
-    'prettier'
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:jsx-a11y/recommended', 'prettier', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'scripts'],
   parser: '@typescript-eslint/parser',
   plugins: [],
